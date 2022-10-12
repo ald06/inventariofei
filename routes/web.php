262e33935d1d0a010dd9794f0f20dad5a757f91f
ald06 <ald_rz_06@hotmail.com>
2022-10-11 22:06:50 -0500

    config initial

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//rutas de prueba
+Route::get('/registrarequipo', function () {
+    return view('equiposComputo.create');
+});
