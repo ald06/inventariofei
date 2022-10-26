466ab49b9a3c04f0bbb0d30dfa7b2ca7ce581293
ald <ald_rz_06@hotmail.com>
2022-10-26 14:12:02 -0500

    created all model with controller and migratios

---
+++ app/Http/Controllers/SoftwareInstaladoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SoftwareInstalado;
+use Illuminate\Http\Request;
+
+class SoftwareInstaladoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SoftwareInstalado  $softwareInstalado
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SoftwareInstalado $softwareInstalado)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SoftwareInstalado  $softwareInstalado
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SoftwareInstalado $softwareInstalado)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SoftwareInstalado  $softwareInstalado
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SoftwareInstalado $softwareInstalado)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SoftwareInstalado  $softwareInstalado
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SoftwareInstalado $softwareInstalado)
+    {
+        //
+    }
+}
