be83733295e2e022ba96b87a668ac509b0ba6dd2
UrielVell <urielvh84@gmail.com>
2022-12-10 18:05:05 -0600

    login estilizado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,42 +14,16 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: rgb(188, 204, 212);
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
             }
-
             .full-height {
                 height: 100vh;
             }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -66,18 +40,22 @@
         </style>
     </head>
     <body>
-      <div class="container">
-          <div class="row justify-content-center">
+      <div class="container py-5" >
+        <div class="card" style="width: 100%;">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img src="{{asset('img/feiuv.png')}}" class="img-fluid rounded-start" alt="...">
+              </div>
               <div class="col-md-8">
-                  <div class="card">
-                      <div class="card-header">{{ __('Login') }}</div>
-
+                <div class="card" style="height: 100%">
+                    <div class="card-header text-center">
+                        <h5 class="card-title">Sistema de Inventario para los Centros de Computo de la FEI</h5>
+                    </div>
                       <div class="card-body">
                           <form method="POST" action="{{ route('login') }}">
                               @csrf
-
                               <div class="form-group row">
-                                  <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                  <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo Electronico') }}</label>
 
                                   <div class="col-md-6">
                                       <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -91,7 +69,7 @@
                               </div>
 
                               <div class="form-group row">
-                                  <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                  <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
                                   <div class="col-md-6">
                                       <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -110,7 +88,7 @@
                                           <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                           <label class="form-check-label" for="remember">
-                                              {{ __('Remember Me') }}
+                                              {{ __('Recordarme') }}
                                           </label>
                                       </div>
                                   </div>
@@ -119,12 +97,12 @@
                               <div class="form-group row mb-0">
                                   <div class="col-md-8 offset-md-4">
                                       <button type="submit" class="btn btn-primary">
-                                          {{ __('Login') }}
+                                          {{ __('Entrar') }}
                                       </button>
 
                                       @if (Route::has('password.request'))
                                           <a class="btn btn-link" href="{{ route('password.request') }}">
-                                              {{ __('Forgot Your Password?') }}
+                                              {{ __('¿Olvidaste tu contraseña?') }}
                                           </a>
                                       @endif
                                   </div>
@@ -133,6 +111,11 @@
                       </div>
                   </div>
               </div>
+              </div>
+            </div>
+          </div>
+          <div class="row justify-content-center">
+              
           </div>
       </div>  </div>
     </body>
